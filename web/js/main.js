caad166c79cab6566905cbf2f0dbd37d8259302f
RunaUXLabs <runa.uxlabs@gmail.com>
2021-07-08 15:56:10 +0900

    디렉토리 변경

--- web/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$(".more_open a").on("click", function(){
-		$(this).hide();
-		$(".more_cts."+$(this).attr("data-name")).slideDown();
-	});
-});
--- web/js/web.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$(".schedule_show").on("click", function(){
-		$(".schedule_table").show();
-		$(this).hide();
-	});
-});
