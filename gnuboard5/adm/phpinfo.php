dae76f4447cf794df341203226ba0d9c4f1b119c
Runa <runa.uxlabs@gmail.com>
2022-11-11 12:47:42 +0900

    그누보드 제거

--- gnuboard5/adm/phpinfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-$sub_menu = "100500";
-require_once './_common.php';
-
-check_demo();
-
-auth_check_menu($auth, $sub_menu, 'r');
-
-phpinfo();
--- gnuboard5/plugin/social/_common.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once('../../common.php');
-
-// 커뮤니티 사용여부
-if(defined('G5_COMMUNITY_USE') && G5_COMMUNITY_USE === false) {
-    define('_SHOP_', true);
-}
